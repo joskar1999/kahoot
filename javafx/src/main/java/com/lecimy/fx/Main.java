accd572a2820209f744109238fa5db9fa734dc67
joskar1999 <joskar1999@gmail.com>
2020-01-05 18:21:51 +0100

    awaiting page created

--- javafx/src/main/java/com/lecimy/fx/Main.java
+++ javafx/src/main/java/com/lecimy/fx/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/userPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/awaitingPage.fxml"));
         primaryStage.setTitle("lecimy");
         primaryStage.setScene(new Scene(root, 1366, 768));
         primaryStage.setResizable(false);
---
+++ javafx/src/main/java/com/lecimy/fx/controller/AwaitingController.java
@@ -0,0 +1,28 @@
+package main.java.com.lecimy.fx.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AwaitingController implements Initializable {
+
+    @FXML
+    private ImageView startButton;
+
+    @FXML
+    private Text playersAmountText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    void startGame() {
+        System.out.println("start game");
+    }
+}
